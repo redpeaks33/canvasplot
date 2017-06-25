b5d49089fd48e29993b2d14cc55e9003a7a1de1f
redpeaks33 <redpeaks33@gmail.com>
2017-06-25 20:41:32 +0900

    canvas

--- Routing/js/controller.js
+++ Routing/js/controller.js
@@ -3,12 +3,27 @@
 main.controller('MyController', ['$scope', '$timeout', function ($scope, $timeout) {
     $scope.initialize = function()
     {
-        $scope.tabClicked('dygraph');
+        //initializeData(0, 0);
+        initializeDataRandom(0, 0);
+        $scope.tabClicked('tab1');
     };
 
     $scope.tabClicked = function(type)
     {
-        initializeData(0,0);
+
+        if (type == 'tab1')
+        {
+            $scope.tab1 = true;
+            $scope.tab2 = false;
+        }
+        else if (type == 'tab2')
+        {
+            $scope.tab1 = false;
+            $scope.tab2 = true;
+        }
+        //ClearCanvas
+        //Load CurrentTime
+       
     }
 
     //#region initialize data
@@ -20,11 +35,21 @@ main.controller('MyController', ['$scope', '$timeout', function ($scope, $timeou
         for (var i = 0; i < limit; i++)
         {
             $scope.dataPoints.push({
-                //t: 0.002 * i,
-                //x: i % 250 - dX,
+                t: 0.002 * i,
                 x: i % 10000 - dY,
-                y: i * 0.00001  - dY,
-                //y: Math.floor((Math.random() * 10000) + 1) - dY,
+                //y: i * 0.00001  - dY,
+                y: Math.floor((Math.random() * 10000) + 1) - dY,
+            });
+        }
+    }
+    var initializeDataRandom = function (dX, dY) {
+        var limit = 32786;    //increase number of dataPoints by increasing this
+        $scope.dataPoints = [];
+        for (var i = 0; i < limit; i++) {
+            $scope.dataPoints.push({
+                t: 0.002 * i,
+                x: Math.floor((Math.random() * 250) + 1),
+                y: Math.floor((Math.random() * 150) + 1),
             });
         }
     }
